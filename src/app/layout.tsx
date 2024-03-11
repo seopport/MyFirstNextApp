33eaaba96af9f25e5936fe8a7861a359cab6f7a0
Jiwon <tj1tj2tj3@naver.com>
2024-03-11 22:27:55 +0900

    스타일링

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,14 +17,69 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>
-        <nav>
-          <Link href='/'>Home </Link>
-          <Link href='/rendering/CSR'>CSR </Link>
-          <Link href='/rendering/SSG'>SSG </Link>
-          <Link href='/rendering/ISR'>ISR </Link>
-          <Link href='/rendering/SSR'>SSR </Link>
-        </nav>
+      <body className='bg-purple-50'>
+        <div className='relative isolate flex items-center justify-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5'>
+          <div
+            className='absolute left-[max(-7rem,calc(50%-52rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl'
+            aria-hidden='true'
+          >
+            <div
+              className='aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30'
+              style={{
+                clipPath:
+                  'polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)',
+              }}
+            />
+          </div>
+          <div
+            className='absolute left-[max(45rem,calc(50%+8rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl'
+            aria-hidden='true'
+          >
+            <div
+              className='aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30'
+              style={{
+                clipPath:
+                  'polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)',
+              }}
+            />
+          </div>
+          <div className='flex flex-wrap items-center gap-x-4 gap-y-2'>
+            <strong className='font-semibold'>
+              <Link href='/'>Home </Link>
+            </strong>
+            <svg viewBox='0 0 2 2' className='mx-2 inline h-0.5 w-0.5 fill-current' aria-hidden='true'>
+              <circle cx={1} cy={1} r={1} />
+            </svg>
+            <strong className='font-semibold'>
+              <Link href='/rendering/CSR'>CSR </Link>
+            </strong>
+            <svg viewBox='0 0 2 2' className='mx-2 inline h-0.5 w-0.5 fill-current' aria-hidden='true'>
+              <circle cx={1} cy={1} r={1} />
+            </svg>
+            <strong className='font-semibold'>
+              <Link href='/rendering/SSG'>SSG </Link>
+            </strong>
+            <svg viewBox='0 0 2 2' className='mx-2 inline h-0.5 w-0.5 fill-current' aria-hidden='true'>
+              <circle cx={1} cy={1} r={1} />
+            </svg>
+            <strong className='font-semibold'>
+              <Link href='/rendering/ISR'>ISR </Link>
+            </strong>
+            <svg viewBox='0 0 2 2' className='mx-2 inline h-0.5 w-0.5 fill-current' aria-hidden='true'>
+              <circle cx={1} cy={1} r={1} />
+            </svg>
+            <strong className='font-semibold'>
+              <Link href='/rendering/SSR'>SSR </Link>
+            </strong>
+          </div>
+          <Link
+            href='https://github.com/seopport'
+            target='_blank'
+            className='flex justify-self-end rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900'
+          >
+            @seopport <span aria-hidden='true'></span>
+          </Link>
+        </div>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,81 @@
 import Image from 'next/image';
 
 export default function Home() {
-  return <main className='flex min-h-screen flex-col items-center justify-center p-24'>여기는 Home입니다.</main>;
+  return (
+    <main>
+      <div className='flex flex-col justify-center items-center text-2xl font-light'>
+        🤓 Next.js의 렌더링 기법을 이해해봅시다. 👽
+      </div>
+      <div className='flex min-h-screen flex-row items-center justify-center p-24 gap-x-10 '>
+        <div>
+          CSR
+          <ul>
+            <div>
+              <li className='list-disc'>
+                정의: CSR은 웹 애플리케이션의 렌더링이 사용자의 브라우저(클라이언트 측)에서 일어나는 방식이다.
+                서버에서는 HTML, CSS, JavaScript 등의 정적 파일만 전송하고, 모든 렌더링은 클라이언트 측에서 JavaScript를
+                이용해 수행된다.
+              </li>
+              <li className='list-disc'>
+                장점: 한 번의 페이지 로드 이후에는 사용자 인터랙션에 빠르게 응답할 수 있으며, 동적인 사용자 경험 제공이
+                용이하다.
+              </li>
+              <li className='list-disc'>단점: 초기 로딩 시간(TTV)이 길 수 있으며, SEO에 불리할 수 있다.</li>
+            </div>
+          </ul>
+        </div>
+        <div>
+          SSG
+          <ul>
+            <div>
+              <li className='list-disc'>
+                정의: SSG는 빌드 타임에 사이트의 모든 페이지를 미리 생성하여 정적 파일로 서버에 저장하는 방식이다. 이
+                파일들은 요청이 있을 때 그대로 클라이언트에게 HTML 파일을 전달함. 최초 빌드시에만 생성된다.
+              </li>
+              <li className='list-disc'>장점: 페이지 로딩 속도(TTV)가 빠르다. SEO가 유리하다.</li>
+              <li className='list-disc'>
+                단점: 초사이트에 동적 요소가 필요한 경우 제한적일 수 있으며, 사이트 업데이트를 위해서는 전체 사이트를
+                다시 빌드해야 한다.
+              </li>
+            </div>
+          </ul>
+        </div>
+        <div>
+          ISR
+          <ul>
+            <div>
+              <li className='list-disc'>
+                정의: ISR은 빌드 시 생성된 정적 페이지를 사용하되, 페이지 요청이 있을 때만 특정 페이지를 재생성하는
+                방식이다.
+              </li>
+              <li className='list-disc'>
+                장점: 정적 사이트의 빠른 로딩 속도와 SSR의 유연성을 결합했다. 설정한 주기마다 페이지를 재생성한다.
+              </li>
+              <li className='list-disc'>
+                단점: 모든 페이지가 항상 최신 상태가 아닐 수 있기 때문에 동적인 콘텐츠를 다루기에 한계가 있다.
+              </li>
+            </div>
+          </ul>
+        </div>
+        <div>
+          SSR
+          <ul>
+            <div>
+              <li className='list-disc'>
+                정의: SSR은 서버에서 페이지의 HTML을 렌더링하여 사용자에게 전송하는 방식이다. 이는 사용자의 요청에 따라
+                서버에서 각 페이지를 실시간으로 생성한다. 새로운 페이지 요청이 있을 때마다 서버에 요청하고 새로운
+                페이지를 로드
+              </li>
+              <li className='list-disc'>
+                장점: 초기 로딩 시간이 빠르고 (껍데기만 먼저 주고 hydration) , 검색 엔진 최적화에 유리하다.
+              </li>
+              <li className='list-disc'>
+                단점: 사용자의 각 요청마다 페이지를 새로 렌더링해야 하므로 서버 부하가 증가할 수 있다.
+              </li>
+            </div>
+          </ul>
+        </div>
+      </div>
+    </main>
+  );
 }
